569a25daed0babbe521f09f3b18ad2ecbe19a0e3
nk <nk@pop-os.localdomain>
2020-12-05 14:50:41 -0600

    basic setup

---
+++ dmhelper/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <testing></testing>
+</template>
+<script>
+import testing from '@/components/testing.vue'
+export default {
+  components: {
+    testing
+  },
+  data: () => ({
+    showAlert: false
+  })
+}
+</script>
