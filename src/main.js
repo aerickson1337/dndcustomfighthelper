59810cb46938aadc464c5da70fb6b3e488bee541
nk <nk@pop-os.localdomain>
2021-12-05 23:24:50 -0600

    fix: delete old files, fix terrible imports

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import store from '@/store/index.js'
 import './plugins/bootstrap-vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import "bootswatch/dist/darkly/bootstrap.min.css";
 import App from './App.vue' // app always last
 import VueSweetalert2 from 'vue-sweetalert2';
@@ -15,8 +14,6 @@ const options = {
 };
 
 Vue.use(VueSweetalert2, options)
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
